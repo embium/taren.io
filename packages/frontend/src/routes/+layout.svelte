0154ccd0bed959e45556b5ae0acdbeb9d190b7c0
Michael Mooney <mikeymooney1991@gmail.com>
2026-08-10 14:53:27 -0500

    Add Google Analytics

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 
 	import { onMount } from 'svelte';
 	import { ModeWatcher } from 'mode-watcher';
+	import { browser } from '$app/environment';
 
 	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from '$lib/components/ui/sonner';
@@ -16,7 +17,18 @@
 	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	{#if browser}
+    <script async src="https://googletagmanager.com"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-CV8Q86TPGN');
+    </script>
+	{/if}
+</svelte:head>
 
 <ModeWatcher />
 <Toaster />
